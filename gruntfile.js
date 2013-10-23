dfe0375f9fca9d84370609752b772c54d21b5901
kristoferjoseph <kristoferjoseph@gmail.com>
2013-10-23 16:13:24 -0700

    Fixing lint errors. Ignoring node_modules

--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,21 @@
-var npmcss = require('npm-css')
-,   path   = require('path')
-,   _      = require('lodash')
-,   fs     = require('fs')
+var npmcss = require('npm-css'),
+    path = require('path'),
+    _ = require('lodash'),
+    fs = require('fs');
 
 
-module.exports = function (grunt) {
-    
+module.exports = function(grunt) {
+
     grunt.initConfig({
 
         ejs: {
             index: {
-                src:'src/index.ejs.html',
-                dest:'public/index.html'    
+                src: 'src/index.ejs.html',
+                dest: 'public/index.html'
             },
             options: {
-                title:'The big title',
-                dev:true
+                title: 'The big title',
+                dev: true
             }
         },
 
@@ -29,8 +29,10 @@ module.exports = function (grunt) {
             }
         },
 
-        uglify: { 
-            options: {mangle:false},
+        uglify: {
+            options: {
+                mangle: false
+            },
             min: {
                 files: {
                     'public/index.min.js': ['public/index.js']
@@ -41,7 +43,7 @@ module.exports = function (grunt) {
         cssmin: {
             combine: {
                 files: {
-                    'public/index.min.css':['public/index.css']
+                    'public/index.min.css': ['public/index.css']
                 }
             }
         },
@@ -52,46 +54,45 @@ module.exports = function (grunt) {
                 tasks: ['default'],
                 options: {
                     spawn: true,
-                    livereload:true
+                    livereload: true
                 },
             },
         },
 
         npmcss: {}
 
-    // The End. Of Config.
-    })
-  
+        // The End. Of Config.
+    });
+
     // 3rd party infra
-    grunt.loadNpmTasks('grunt-browserify')
-    grunt.loadNpmTasks('grunt-contrib-uglify')
-    grunt.loadNpmTasks('grunt-contrib-cssmin')
-    grunt.loadNpmTasks('grunt-contrib-watch')
-   
-    // main entry points 
-    grunt.registerTask('default', ['ejs:index', 'browserify', 'npmcss'])
-    grunt.registerTask('release', ['default', 'uglify:min', 'cssmin'])
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // main entry points
+    grunt.registerTask('default', ['ejs:index', 'browserify', 'npmcss']);
+    grunt.registerTask('release', ['default', 'uglify:min', 'cssmin']);
 
     // FIXME these tasks should become modules in their own right
     grunt.registerTask('npmcss', [], function() {
-        var css = npmcss(path.join(__dirname, 'src', 'css', 'index.css'))
-        require('fs').writeFileSync(path.join(__dirname, 'public', 'index.css'), css)
-        grunt.log.ok('Bundled public/index.css')
-    })
+        var css = npmcss(path.join(__dirname, 'src', 'css', 'index.css'));
+        require('fs').writeFileSync(path.join(__dirname, 'public', 'index.css'), css);
+        grunt.log.ok('Bundled public/index.css');
+    });
 
     grunt.registerTask('ejs', 'Compile the index.ejs.html file.', function(arg1) {
-        
-        grunt.config.requires('ejs.index')
-        grunt.config.requires('ejs.options')
-    
-        var index = grunt.config('ejs.index') 
-        ,   src   = index.src
-        ,   dest  = index.dest
-        ,   txt   = fs.readFileSync(path.join(__dirname, src)).toString()
-        ,   out   = _.template(txt, grunt.config('ejs.options'))
-        
-        grunt.log.ok('Bundled public/index.html')
-        fs.writeFileSync(dest, out)
-    })
-// The End.  
-}
+
+        grunt.config.requires('ejs.index');
+        grunt.config.requires('ejs.options');
+
+        var index = grunt.config('ejs.index'),
+            src = index.src,
+            dest = index.dest,
+            txt = fs.readFileSync(path.join(__dirname, src)).toString(),
+            out = _.template(txt, grunt.config('ejs.options'));
+
+        grunt.log.ok('Bundled public/index.html');
+        fs.writeFileSync(dest, out);
+    });
+};
